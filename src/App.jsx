5887a07bdbc40f5c209e0f6b892c875c1b7e16ad
Shipra <Shiprarah@gmail.com>
2025-08-08 23:17:56 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Shipralogo from "./assets/Shipralogo.svg";
 function App() {
   return (
     <div>
-      {/* Sticky navbar at top */}
-
+     
       {/* Vertically centered main content */}
       <div className="h-[calc(100vh-4rem)] w-full flex flex-col justify-center items-center">
         <Home />
